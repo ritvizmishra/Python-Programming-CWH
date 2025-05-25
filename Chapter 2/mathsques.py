d0483d5f0dbc7109201c50fb4935864e759ef217
Ritviz <1822mritviz@gmail.com>
2025-05-25 20:54:25 +0530

    add chapter 2

---
+++ Chapter 2/mathsques.py
@@ -0,0 +1,6 @@
+a = int(input("Enter your number: "))
+print(f"The remainder on dividing with z is {a%2}.")
+
+b = int(input("Enter the number you want a square of: "))
+square = b ** 2
+print(square)
